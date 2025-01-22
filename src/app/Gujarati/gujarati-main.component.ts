14d6129bbbcea9457a08ab3d34c3608684c36f8d
Dev <Dev7202888811@gmail.com>
2025-01-23 00:51:47 +0530

    c

---
+++ src/app/Gujarati/gujarati-main.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-gujarati-main',
+  standalone: true,
+  imports: [],
+  templateUrl: './gujarati-main.component.html',
+  styleUrl: './gujarati-main.component.css'
+})
+export class GujaratiMainComponent {
+
+}
